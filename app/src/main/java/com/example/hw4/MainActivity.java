46208b90760b2d2ca122230c9e13870402f9bd40
hikaruozeki <ozeki@umich.edu>
2019-11-19 12:46:16 -0500

    First Commit

--- app/src/main/java/com/example/hw4/MainActivity.java
+++ app/src/main/java/com/example/hw4/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -42,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
+    
+
+
 
     @Override
     public void onClick(View v) {
